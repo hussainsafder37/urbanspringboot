629d50a82a800ff510258c5e41200da56cd30fb6
Hussain <hussainsafder37@gmail.com>
2020-06-20 12:32:21 +0500

    commit

---
+++ src/main/java/com/hussain/service/ShoppingCartService.java
@@ -0,0 +1,20 @@
+package com.hussain.service;
+
+import com.reljicd.exception.NotEnoughProductsInStockException;
+import com.reljicd.model.Product;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+public interface ShoppingCartService {
+
+    void addProduct(Product product);
+
+    void removeProduct(Product product);
+
+    Map<Product, Integer> getProductsInCart();
+
+    void checkout() throws NotEnoughProductsInStockException;
+
+    BigDecimal getTotal();
+}
